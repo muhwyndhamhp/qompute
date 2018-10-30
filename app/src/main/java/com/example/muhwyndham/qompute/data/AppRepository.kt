6ea3e4244306714ff6f1a74c0d6424af788f4656
muhwyndhamhp <wyndham.haryata.p@mail.ugm.ac.id>
2018-10-30 13:40:03 +0700

    adding class constructor to app repository

--- app/src/main/java/com/example/muhwyndham/qompute/data/AppRepository.kt
+++ app/src/main/java/com/example/muhwyndham/qompute/data/AppRepository.kt
@@ -1,2 +1,9 @@
 package com.example.muhwyndham.qompute.data
 
+import com.example.muhwyndham.qompute.data.model.ComponentDao
+
+class AppRepository private constructor(
+    componentDao: ComponentDao
+) {
+
+}
