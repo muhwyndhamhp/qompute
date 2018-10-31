2a20c38a1c597d22037946e84f245219e9d9fe88
muhwyndhamhp <wyndham.haryata.p@mail.ugm.ac.id>
2018-10-31 07:22:38 +0700

    add functions on repository to fetch data, either from local and from api endpoint

--- app/src/main/java/com/example/muhwyndham/qompute/data/AppRepository.kt
+++ app/src/main/java/com/example/muhwyndham/qompute/data/AppRepository.kt
@@ -1,9 +1,57 @@
 package com.example.muhwyndham.qompute.data
 
+import com.example.muhwyndham.qompute.data.model.Component
 import com.example.muhwyndham.qompute.data.model.ComponentDao
+import com.example.muhwyndham.qompute.network.service.NetworkService
 
 class AppRepository private constructor(
-    componentDao: ComponentDao
+    private  val componentDao: ComponentDao
 ) {
+
+    interface LoadDataCallback{
+        fun onFailed(TAG: String, t: Throwable)
+        fun onSuccess(components: List<Component>)
+    }
+
+    fun getAllComponents() = componentDao.getAllComponents()
+
+    fun getComponentsPaged() = componentDao.getComponentsPaged()
+
+    fun getSingleComponent(id: Int) = componentDao.getSingleComponent(id)
+
+    fun getSingleComponent(name: String) = componentDao.getSingleComponent(name)
+
+    fun reloadData1(loadDataCallback: LoadDataCallback){
+        val networkService = NetworkService.getInstance()
+
+        val call = networkService.getAll1().subscribe({
+            componentList ->
+            componentDao.insertAll(componentList)
+            loadDataCallback.onSuccess(componentList)
+        }){
+            loadDataCallback.onFailed(TAG, it)
+        }
+    }
+
+    fun reloadData2(loadDataCallback: LoadDataCallback){
+        val networkService = NetworkService.getInstance()
+
+        val call = networkService.getAll2().subscribe({
+            componentList ->
+            componentDao.insertAll(componentList)
+            loadDataCallback.onSuccess(componentList)
+        }){
+            loadDataCallback.onFailed(TAG, it)
+        }
+    }
+
+    companion object {
+        private const val TAG = "REPO"
+
+        @Volatile
+        private var instance: AppRepository? = null
+        fun getInstance(componentDao: ComponentDao) = instance ?: synchronized(this){ instance ?: AppRepository(componentDao).also  { instance = it }}
+    }
+
 
 }
