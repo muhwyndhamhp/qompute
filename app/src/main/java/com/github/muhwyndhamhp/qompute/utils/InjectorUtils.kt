d0183966ae1c00b7b019bcd9476cfc2a55dd8b04
muhwyndhamhp <wyndham.haryata.p@mail.ugm.ac.id>
2018-10-31 21:15:09 +0700

    move viewmodel factory to package viewmodel/factory

--- app/src/main/java/com/github/muhwyndhamhp/qompute/utils/InjectorUtils.kt
+++ app/src/main/java/com/github/muhwyndhamhp/qompute/utils/InjectorUtils.kt
@@ -3,14 +3,15 @@ package com.github.muhwyndhamhp.qompute.utils
 import android.content.Context
 import com.github.muhwyndhamhp.qompute.data.AppRepository
 import com.github.muhwyndhamhp.qompute.data.database.AppDatabase
-import com.github.muhwyndhamhp.qompute.viewmodel.MainViewModelFactory
+import com.github.muhwyndhamhp.qompute.viewmodel.factory.MainViewModelFactory
 
 object InjectorUtils {
 
     private fun getAppRepository(context: Context) = AppRepository.getInstance(
-        AppDatabase.getInstance(context).componentDao())
+        AppDatabase.getInstance(context).componentDao(),
+        AppDatabase.getInstance(context).buildDao())
 
-    fun provideMainViewModelFactory(context: Context): MainViewModelFactory{
+    fun provideMainViewModelFactory(context: Context): MainViewModelFactory {
         val repository = getAppRepository(context)
         return MainViewModelFactory(repository)
     }
--- app/src/main/java/com/github/muhwyndhamhp/qompute/viewmodel/factory/MainViewModelFactory.kt
+++ app/src/main/java/com/github/muhwyndhamhp/qompute/viewmodel/factory/MainViewModelFactory.kt
@@ -1,8 +1,9 @@
-package com.github.muhwyndhamhp.qompute.viewmodel
+package com.github.muhwyndhamhp.qompute.viewmodel.factory
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.github.muhwyndhamhp.qompute.data.AppRepository
+import com.github.muhwyndhamhp.qompute.viewmodel.MainViewModel
 
 class MainViewModelFactory(val repository: AppRepository) : ViewModelProvider.NewInstanceFactory(){
 
