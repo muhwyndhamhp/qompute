bb1d2013acc1650280681d9b86cadf85d2c294a8
muhwyndhamhp <wyndham.haryata.p@mail.ugm.ac.id>
2018-11-01 10:11:30 +0700

    clean main activity

--- app/src/main/java/com/github/muhwyndhamhp/qompute/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/github/muhwyndhamhp/qompute/viewmodel/MainViewModel.kt
@@ -8,22 +8,4 @@ import com.github.muhwyndhamhp.qompute.utils.ERROR_CODE_FAILED_TO_FETCH
 
 class MainViewModel(val repository: AppRepository): ViewModel(){
 
-    val componentList: MutableLiveData<List<Component>> = MutableLiveData()
-    val exceptionList : MutableLiveData<MutableList<String>> = MutableLiveData()
-
-    fun LoadAllComponents(){
-        repository.reloadData1(object : AppRepository.LoadDataCallback{
-            override fun onFailed(TAG: String, t: Throwable) {
-                val exceptions: MutableList<String>? = exceptionList.value
-                exceptions?.set(ERROR_CODE_FAILED_TO_FETCH, t.message!!)
-                exceptionList.value = exceptions
-            }
-
-            override fun onSuccess(components: List<Component>) {
-                componentList.value = components
-            }
-
-        })
-    }
-
 }
